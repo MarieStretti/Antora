8f649d92e510159c82b316de72fea2a8d0041d04
Dan Allen <dan@opendevise.com>
2017-09-22 19:37:26 -0600

    add lint task to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 const gulp = require('gulp')
 
 const build = require('./tasks/build-task')
+const lint = require('./tasks/lint-task')
 
 gulp.task('build', build)
+gulp.task('lint', () => lint(['lib*/**/*.js', 'test/**/*.js']))
 gulp.task('default', ['build'])
---
+++ tasks/lint-task.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const vfs = require('vinyl-fs')
+const standard = require('gulp-standard')
+
+module.exports = (glob) =>
+  vfs.src(glob)
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true
+    }))
