3cc6d0b4278b6f064af9daf96f605ee98da4599a
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-09-26 15:39:14 +0200

    remove "lint" script and use eslint (+ standard rules) in the gulp task (instead of gulp-standard)

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,13 @@ const lint = require('./tasks/lint-task')
 const test = require('./tasks/test-task')
 
 const testFiles = ['test/**/*-test.js', 'packages/*/test/**/*-test.js']
+const allFiles = [
+  'gulpfile.js',
+  '{lib*,tasks,test}/**/*.js',
+  'packages/*/{lib,test}/**/*.js'
+]
 
-gulp.task('lint', () =>
-  lint([
-    'gulpfile.js',
-    '{lib*,tasks,test}/**/*.js',
-    'packages/*/{lib,test}/**/*.js'
-  ])
-)
-
+gulp.task('lint', () => lint(allFiles))
 gulp.task('test', ['lint'], () => test(testFiles))
 gulp.task('test-only', () => test(testFiles))
 gulp.task('test-watch', () => gulp.watch(allFiles, ['test-only']))
