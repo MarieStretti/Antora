0962a4b842a1671a515193a46355b073abe96a5f
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-09-28 18:43:38 +0200

    apply playbook architecture and acceptance tests

--- packages/playbook/lib/config/schema.js
+++ packages/playbook/lib/config/schema.js
@@ -4,117 +4,130 @@ module.exports = {
     format: String,
     default: 'site.yml',
     env: 'PLAYBOOK',
-    arg: 'playbook'
-  },
-  quiet: {
-    doc: 'Do not write any messages to stdout.',
-    format: Boolean,
-    default: false,
-    arg: 'quiet'
-  },
-  silent: {
-    doc: 'Suppress all messages.',
-    format: Boolean,
-    default: false,
-    arg: 'silent'
+    arg: 'playbook',
   },
   site: {
     url: {
-      doc: 'The base URL of the published site (optional). Should not include a trailing slash.',
+      doc:
+        'The base URL of the published site (optional). Should not include a trailing slash.',
       format: String,
       default: undefined,
       env: 'URL',
-      arg: 'url'
+      arg: 'url',
     },
     title: {
       doc: 'The title of the site (optional).',
       format: String,
       default: undefined,
-      arg: 'title'
+      arg: 'title',
     },
     root: {
-      doc: 'The name of the component to use as the root of the site (optional).',
+      doc:
+        'The name of the component to use as the root of the site (optional).',
       format: String,
-      default: undefined
+      default: undefined,
     },
     aspect: {
-      doc: 'The name of the aspect navigation to make available on every page in the site.',
+      doc:
+        'The name of the aspect navigation to make available on every page in the site.',
       format: String,
-      default: undefined
+      default: undefined,
     },
     nav: {
-      doc: 'The list of descriptors which define the aspect navigation domains.',
+      doc:
+        'The list of descriptors which define the aspect navigation domains.',
       format: Array,
-      default: undefined
+      default: undefined,
     },
     keys: {
       google_analytics: {
         doc: 'The Google Analytics account key.',
         format: String,
         default: undefined,
-        arg: 'google-analytics-key'
+        arg: 'google-analytics-key',
       },
       swiftype: {
         doc: 'The key to activate the SwiftType widget.',
         format: String,
         default: undefined,
-        arg: 'swiftype-key'
-      }
-    }
+        arg: 'swiftype-key',
+      },
+    },
   },
   content: {
     sources: {
       doc: 'The list of git repositories + branch patterns to use.',
       format: Array,
       default: [],
-      env: 'CONTENT_SOURCES'
-    }
+      env: 'CONTENT_SOURCES',
+    },
   },
   ui: {
     location: {
       doc: 'The repository that hosts the UI.',
       format: String,
-      default: undefined
+      default: undefined,
     },
     name: {
       doc: 'The name of the UI bundle. Defaults to the repository name.',
       format: String,
-      default: undefined
+      default: undefined,
     },
     ref: {
       doc: 'The reference (or version) of the theme bundle to use.',
       format: String,
-      default: undefined
+      default: undefined,
     },
     archive: {
-      doc: 'A local theme archive. If specified, used in place of the UI bundle from the repository.',
+      doc:
+        'A local theme archive. If specified, used in place of the UI bundle from the repository.',
       format: String,
       default: undefined,
-      arg: 'ui-archive'
+      arg: 'ui-archive',
     },
     skip_cache: {
-      doc: 'Skip the local bundle cache and always fetch the UI bundle from the repository.',
+      doc:
+        'Skip the local bundle cache and always fetch the UI bundle from the repository.',
       format: Boolean,
       default: false,
-      arg: 'skip-ui-cache'
-    }
+      arg: 'skip-ui-cache',
+    },
   },
-  html_url_extension_strategy: {
-    doc: 'Controls how the URL extension for HTML pages is handled (default, drop, or indexify).',
-    format: String,
-    default: 'default',
-    arg: 'html-url-extension-strategy'
+  runtime: {
+    quiet: {
+      doc: 'Do not write any messages to stdout.',
+      format: Boolean,
+      default: false,
+      arg: 'quiet',
+    },
+    silent: {
+      doc: 'Suppress all messages.',
+      format: Boolean,
+      default: false,
+      arg: 'silent',
+    },
   },
-  aspect_page_url_strategy: {
-    doc: 'Controls how links to pages in aspect domains are generated (path or query).',
-    format: String,
-    default: 'path',
-    arg: 'aspect-page-url-strategy'
+  urls: {
+    htmlExtensionStyle: {
+      doc:
+        'Controls how the URL extension for HTML pages is handled (default, drop, or indexify).',
+      format: ['default', 'drop', 'indexify'],
+      default: 'default',
+      arg: 'html-url-extension-style',
+    },
+    aspectPageStrategy: {
+      doc:
+        'Controls how links to pages in aspect domains are generated (path or query).',
+      format: String,
+      default: 'path',
+      arg: 'aspect-page-url-strategy',
+    },
   },
   redirects: {
-    doc: 'Generate nginx config file containing URL redirects for page aliases.',
+    doc:
+      'Generate nginx config file containing URL redirects for page aliases.',
     format: Boolean,
     default: false,
-    arg: 'redirects'
-  }
+    arg: 'redirects',
+  },
 }
