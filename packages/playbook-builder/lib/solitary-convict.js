a1ebe6cfa20a44ab4c1c3a709015ba4f6f1edfc7
Dan Allen <dan@opendevise.com>
2017-12-03 19:37:05 -0700

    isolate playbook builder from process environment
    - wrap the convict function to isolate it from the environment
    - add args (array) and env (map) arguments to buildPlaybook() function
    - update tests to pass args and env as arguments to buildPlaybook() function
    - specify in guidebook that args and env must be passed to buildPlaybook()
    - clarify in guidebook that playbook spec file is relative to process working directory
    - state in guidebook that playbook spec file has implicit .yml extension if missing extension

---
+++ packages/playbook-builder/lib/solitary-convict.js
@@ -0,0 +1,36 @@
+const convict = require('convict')
+
+/**
+ * A convict function wrapper that decouples it from the process environment.
+ * This wrapper allows the args array and env map to be specified as options.
+ */
+module.exports = (schema, opts = {}) => {
+  let processArgv
+  let args = opts.args || []
+  processArgv = process.argv
+  // NOTE convict expects first two arguments to be node command and script filename
+  let argv = processArgv.slice(0, 2).concat(args)
+  process.argv = argv
+
+  let processEnv
+  let env = opts.env || {}
+  processEnv = process.env
+  process.env = env
+
+  const config = convict(schema)
+
+  process.argv = processArgv
+  process.env = processEnv
+
+  const originalLoad = config.load
+  config.load = function (configOverlay) {
+    process.argv = argv
+    process.env = env
+    const combinedConfig = originalLoad.apply(this, [configOverlay])
+    process.argv = processArgv
+    process.env = processEnv
+    return combinedConfig
+  }
+
+  return config
+}
