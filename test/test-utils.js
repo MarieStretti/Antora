10bd4e19f8314bc0ead82f955960665635bdbeb7
Dan Allen <dan@opendevise.com>
2017-12-17 02:40:48 -0700

    patch chai to skip internal Opal references when traversing objects

--- test/test-utils.js
+++ test/test-utils.js
@@ -3,7 +3,26 @@
 // IMPORTANT nodegit must be loaded before asciidoctor.js or else promisify gets tripped up by Opal enhancements
 require('nodegit')
 
+const interceptRequire = require('intercept-require')
+const patchChai = (_, info) => {
+  if (
+    info.moduleId.endsWith('/getEnumerableProperties') &&
+    (info.absPath || `/node_modules/${info.moduleId}`).endsWith(
+      '/node_modules/chai/lib/chai/utils/getEnumerableProperties.js'
+    )
+  ) {
+    return (obj) => {
+      const props = []
+      for (let prop in obj) {
+        if (!prop.startsWith('$')) props.push(prop)
+      }
+      return props
+    }
+  }
+}
+const uninterceptRequire = interceptRequire(patchChai)
 const chai = require('chai')
+uninterceptRequire()
 const expect = chai.expect
 
 const chaiAsPromised = require('chai-as-promised')
