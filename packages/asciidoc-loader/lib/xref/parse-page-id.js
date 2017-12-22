4add3bd082d840443d608c39a3f0bc956f710591
Dan Allen <dan@opendevise.com>
2017-12-22 13:18:57 -0700

    replace src.subpath with src.relative in asciidoc-loader component
    - don't split out subpath (i.e., topic dir) in parsePageId
    - change return value of parsePageId to page ID object (not full src)
    - use src.path and src.basename in resolve include file
    - update tests

--- packages/asciidoc-loader/lib/xref/parse-page-id.js
+++ packages/asciidoc-loader/lib/xref/parse-page-id.js
@@ -2,15 +2,15 @@
 
 // matches pattern version@component:module:topic/page w/ optional .adoc ext
 // ex. 1.0@antora:playbook:ui/bundle.adoc
-const PAGE_ID_RX = /^(?:([^@]+)@)?(?:(?:([^:]+):)?(?:([^:]+))?:)?(?:([^:]+)\/)?([^:]+?)(?:\.adoc)?$/
-const PAGE_ID_RXG = { version: 1, component: 2, module: 3, subpath: 4, stem: 5 }
+const PAGE_ID_RX = /^(?:([^@]+)@)?(?:(?:([^:]+):)?(?:([^:]+))?:)?([^:]+?)(?:\.adoc)?$/
+const PAGE_ID_RXG = { version: 1, component: 2, module: 3, page: 4 }
 
 /**
  * Parses a contextual page ID string into a file src object.
  *
  * Parses the specified contextual page ID string into a file src object. If a
  * context src object is provided, it will be used to populate the component,
- * version, and/or module properties if missing.
+ * version, and/or module properties, if missing.
  *
  * * If a component is specified, but not a version, the version defaults to master.
  * * If a component is specified, but not a module, the module defaults to ROOT.
@@ -21,7 +21,7 @@ const PAGE_ID_RXG = { version: 1, component: 2, module: 3, subpath: 4, stem: 5 }
  *   version@component:module:topic/page followed by optional .adoc ext).
  * @param {Object} [ctx={}] - The src context.
  *
- * @returns {Object} The resolved file src object for this contextual page ID.
+ * @returns {Object} A page ID object that can be used to look up the file in the content catalog.
  */
 function parsePageId (spec, ctx = {}) {
   const match = spec.match(PAGE_ID_RX)
@@ -30,8 +30,7 @@ function parsePageId (spec, ctx = {}) {
   let version = match[PAGE_ID_RXG.version]
   let component = match[PAGE_ID_RXG.component]
   let module = match[PAGE_ID_RXG.module]
-  let subpath = match[PAGE_ID_RXG.subpath] || ''
-  let stem = match[PAGE_ID_RXG.stem]
+  let relative = match[PAGE_ID_RXG.page] + '.adoc'
 
   if (component) {
     // if a component is specified, but not a version, assume version is "master"
@@ -40,17 +39,12 @@ function parsePageId (spec, ctx = {}) {
     if (!module) module = 'ROOT'
   }
 
-  // Q: should this type be a File src object or a page ID?
   return {
     component: component || ctx.component,
     version: version || ctx.version,
     module: module || ctx.module,
     family: 'page',
-    subpath,
-    mediaType: 'text/asciidoc',
-    basename: stem + '.adoc',
-    stem,
-    extname: '.adoc',
+    relative,
   }
 }
 
--- packages/asciidoc-loader/lib/xref/resolve-page.js
+++ packages/asciidoc-loader/lib/xref/resolve-page.js
@@ -1,17 +1,14 @@
 'use strict'
 
-const _ = require('lodash')
 const parsePageId = require('./parse-page-id')
 
-const PAGE_ID_PROPERTY_NAMES = ['component', 'version', 'module', 'family', 'subpath', 'basename']
-
 /**
- * Attempts to resolve a contextual page ID string to a file in the catalog.
+ * Attempts to resolve a contextual page ID spec to a file in the catalog.
  *
- * Parses the specified contextual page ID string into a file src object using
+ * Parses the specified contextual page ID spec into a page ID object using
  * parsePageId, then attempts to locate a file with this page ID in the catalog.
- * If a file cannot be resolved, the function returns undefined. If the string
- * does not match the page ID syntax, the function throws an error.
+ * If a file cannot be resolved, the function returns undefined. If the spec
+ * does not match the page ID syntax, this function throws an error.
  *
  * @memberOf module:asciidoc-loader
  *
@@ -20,15 +17,15 @@ const PAGE_ID_PROPERTY_NAMES = ['component', 'version', 'module', 'family', 'sub
  * @param {ContentCatalog} catalog - The content catalog in which to resolve the page file.
  * @param {Object} [ctx={}] - The src context.
  *
- * @return {File} The virtual file to which the contextual string page ID
+ * @return {File} The virtual file to which the contextual page ID spec
  * refers, or undefined if the file cannot be resolved
  */
 function resolvePage (spec, catalog, ctx = {}) {
-  const pageSrc = parsePageId(spec, ctx)
+  const id = parsePageId(spec, ctx)
 
-  if (!pageSrc) throw new Error('Invalid page ID syntax')
+  if (!id) throw new Error('Invalid page ID syntax')
 
-  return catalog.getById(_.pick(pageSrc, ...PAGE_ID_PROPERTY_NAMES))
+  return catalog.getById(id)
 }
 
 module.exports = resolvePage
