166093eff8331dc7072710dd12e1310a5b759d58
Dan Allen <dan@opendevise.com>
2018-01-01 03:50:25 -0700

    add first draft of default pipeline component

---
+++ packages/pipeline-default/lib/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/**
+ * Default Pipeline component for Antora
+ *
+ * Runs an Antora documentation pipeline using the default set of components.
+ *
+ * @namespace pipeline-default
+ */
+module.exports = require('./pipeline.js')
