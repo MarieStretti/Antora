2801236583251e5e6339a6454a0a31ef1e9b1e8e
Dan Allen <dan@opendevise.com>
2018-01-05 04:13:19 -0700

    generate content repository used in cli tests
    - add RepositoryBuilder test utility
    - use RepositoryBuilder to import fixtures into a dedicated repository for tests

--- packages/cli/test/cli-test.js
+++ packages/cli/test/cli-test.js
@@ -5,14 +5,14 @@ const { expect } = require('../../../test/test-utils')
 
 const fs = require('fs-extra')
 const { default: Kapok } = require('kapok-js')
-const git = require('nodegit')
 const path = require('path')
+const RepositoryBuilder = require('../../../test/repository-builder')
 
 const ANTORA_CLI = path.resolve(__dirname, '../bin/antora.js')
+const CONTENT_REPO_DIR = path.resolve(__dirname, 'content-repo')
 const CWD = process.cwd()
 const FIXTURES_DIR = path.resolve(__dirname, 'fixtures')
 const NODE = process.argv[0]
-const PROJECT_DIR = path.resolve(__dirname, '../../..')
 const TIMEOUT = 5000
 const UI_BUNDLE_URI =
   'https://gitlab.com/antora/antora-ui-default/-/jobs/artifacts/master/raw/build/ui-bundle.zip?job=bundle-stable'
@@ -21,23 +21,18 @@ const WORK_DIR = path.resolve(__dirname, 'work')
 
 describe('cli', () => {
   Kapok.config.shouldShowLog = false
-  let currentBranch
-  let startPath
   let playbookSpec
   let playbookSpecFile
   let destDir
   let uiBundleUri
 
-  const getCurrentBranch = async () => {
-    const repo = await git.Repository.open(PROJECT_DIR)
-    const result = (await repo.getCurrentBranch()).name().replace(/^.+\//, '')
-    repo.free()
-    return result
+  const setUpFixtureRepo = () => {
+    return new RepositoryBuilder(path.dirname(CONTENT_REPO_DIR), path.join(FIXTURES_DIR, 'the-component-1.0'))
+      .init('content-repo').then((builder) => builder.importFixture())
   }
 
   before(async function () {
-    currentBranch = await getCurrentBranch()
-    startPath = path.relative(PROJECT_DIR, path.join(FIXTURES_DIR, 'the-component-1.0'))
+    await setUpFixtureRepo()
     playbookSpecFile = path.join(WORK_DIR, 'the-site.json')
     destDir = 'build/site'
     uiBundleUri = UI_BUNDLE_URI
@@ -49,13 +44,14 @@ describe('cli', () => {
     playbookSpec = {
       site: { title: 'The Site' },
       content: {
-        sources: [{ url: PROJECT_DIR, branches: currentBranch, start_path: startPath }],
+        sources: [{ url: CONTENT_REPO_DIR, branches: 'master' }],
       },
       ui: { bundle: uiBundleUri },
     }
   })
 
   after(() => {
+    fs.removeSync(CONTENT_REPO_DIR)
     if (process.env.KEEP_CACHE) {
       fs.removeSync(path.join(WORK_DIR, destDir.split('/')[0]))
       fs.removeSync(playbookSpecFile)
@@ -67,6 +63,7 @@ describe('cli', () => {
 
   it('should output version when called with "-v"', () => {
     return Kapok
+      // Q: why do we have to specify node and antora?
       .start(NODE, [ANTORA_CLI, '-v'])
       .assert(VERSION)
       .done()
@@ -145,7 +142,7 @@ describe('cli', () => {
       .done()
   })
 
-  it('should generate site to output folder when playbook is passed to the generate command', async () => {
+  it.only('should generate site to output folder when playbook is passed to the generate command', async () => {
     fs.writeJsonSync(playbookSpecFile, playbookSpec)
     // Q: how do we assert w/ kapok when there's no output; use promise as workaround
     await new Promise((resolve) => {
