e7024fd0de6836d231a46fd67fcc1319d6d0c300
Dan Allen <dan@opendevise.com>
2018-01-11 03:37:31 -0700

    rewrite computeRelativeUrlPath function and add tests
    - fixed implementation to handle self-referencing indexified path
    - added a test suite for the function

---
+++ packages/asciidoc-loader/test/compute-relative-url-path-test.js
@@ -0,0 +1,70 @@
+/* eslint-env mocha */
+'use strict'
+
+const { expect } = require('../../../test/test-utils')
+
+const computeRelativeUrlPath = require('@antora/asciidoc-loader/lib/util/compute-relative-url-path')
+
+describe('computeRelativeUrlPath()', () => {
+  describe('file paths with .html extension', () => {
+    it('should compute URL to file in same directory', () => {
+      expect(computeRelativeUrlPath('/dir/from.html', '/dir/to.html')).to.equal('to.html')
+    })
+
+    it('should compute URL to file in subdirectory', () => {
+      expect(computeRelativeUrlPath('/dir/from.html', '/dir/subdir/to.html')).to.equal('subdir/to.html')
+    })
+
+    it('should compute URL to file in parent directory', () => {
+      expect(computeRelativeUrlPath('/dir/subdir/from.html', '/dir/to.html')).to.equal('../to.html')
+    })
+
+    it('should compute URL to self', () => {
+      expect(computeRelativeUrlPath('/dir/file.html', '/dir/file.html')).to.equal('file.html')
+    })
+
+    it('should compute URL to index file', () => {
+      expect(computeRelativeUrlPath('/dir/from.html', '/dir/index.html')).to.equal('index.html')
+    })
+  })
+
+  describe('extensionless file paths', () => {
+    it('should compute URL to file in same directory', () => {
+      expect(computeRelativeUrlPath('/dir/from', '/dir/to')).to.equal('to')
+    })
+
+    it('should compute URL to file in subdirectory', () => {
+      expect(computeRelativeUrlPath('/dir/from', '/dir/subdir/to')).to.equal('subdir/to')
+    })
+
+    it('should compute URL to file in parent directory', () => {
+      expect(computeRelativeUrlPath('/dir/subdir/from', '/dir/to')).to.equal('../to')
+    })
+
+    it('should compute URL to self', () => {
+      expect(computeRelativeUrlPath('/dir/file', '/dir/file')).to.equal('file')
+    })
+  })
+
+  describe('indexified directory paths', () => {
+    it('should compute URL to sibling directory', () => {
+      expect(computeRelativeUrlPath('/dir/from/', '/dir/to/')).to.equal('../to/')
+    })
+
+    it('should compute URL to subdirectory', () => {
+      expect(computeRelativeUrlPath('/dir/from/', '/dir/from/to/')).to.equal('to/')
+    })
+
+    it('should compute URL to parent directory', () => {
+      expect(computeRelativeUrlPath('/dir/from/', '/dir/')).to.equal('../')
+    })
+
+    it('should compute URL to self', () => {
+      expect(computeRelativeUrlPath('/dir/from/', '/dir/from/')).to.equal('./')
+    })
+
+    it('should compute URL to root', () => {
+      expect(computeRelativeUrlPath('/dir/from/', '/')).to.equal('../../')
+    })
+  })
+})
