b1493bb8185caf8cd6025c95cabe8b7bc13be326
Dan Allen <dan@opendevise.com>
2018-01-28 01:14:07 -0700

    make prepublish script resilient to retries

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -2,16 +2,26 @@
 
 const fs = require('fs')
 
-// Transform README.adoc into README.md and hide README.adoc
-fs.readFile('README.adoc', 'utf8', (readErr, asciidoc) => {
-  if (readErr) throw readErr
-  fs.rename('README.adoc', '.README.adoc', (renameErr) => {
-    if (renameErr) throw renameErr
-  })
+function writeMarkdown (asciidoc) {
   const markdown = asciidoc
     .replace(/^=+(?= \w)/gm, (m) => '#'.repeat(m.length))
     .replace(new RegExp('(https?:[^\\[]+)\\[(|.*?[^\\\\])\\]', 'g'), '[$2]($1)')
   fs.writeFile('README.md', markdown, (writeErr) => {
     if (writeErr) throw writeErr
   })
+}
+
+// Transform README.adoc into README.md and hide README.adoc
+fs.readFile('README.adoc', 'utf8', (readErr, asciidoc) => {
+  if (readErr) {
+    fs.readFile('.README.adoc', 'utf8', (retryReadErr, retryAsciidoc) => {
+      if (retryReadErr) throw retryReadErr
+      writeMarkdown(retryAsciidoc)
+    })
+  } else {
+    fs.rename('README.adoc', '.README.adoc', (renameErr) => {
+      if (renameErr) throw renameErr
+    })
+    writeMarkdown(asciidoc)
+  }
 })
