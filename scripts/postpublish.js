0fb5d9610ec180fa15069eece0ccc94c1adfa88c
Dan Allen <dan@opendevise.com>
2018-01-28 03:23:59 -0700

    rewrite pre and post publish scripts to use promises

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -1,11 +1,23 @@
 'use strict'
 
-const fs = require('fs')
+/**
+ * Removes the generated Markdown README (README.md) in the working directory
+ * and restores the hidden AsciiDoc README (.README.adoc -> README.adoc).
+ */
 
-// Remove README.md and restore hidden README.adoc
-fs.unlink('README.md', (unlinkErr) => {
-  if (unlinkErr) throw unlinkErr
-})
-fs.rename('.README.adoc', 'README.adoc', (renameErr) => {
-  if (renameErr) throw renameErr
-})
+const fs = require('fs')
+const { promisify } = require('util')
+
+const README_SRC = 'README.adoc'
+const README_HIDDEN = '.' + README_SRC
+const README_DEST = 'README.md'
+
+;(async () => {
+  const nukeP = promisify(fs.exists)(README_DEST).then((exists) => {
+    if (exists) return promisify(fs.unlink)(README_DEST)
+  })
+  const restoreP = promisify(fs.exists)(README_HIDDEN).then((exists) => {
+    if (exists) return promisify(fs.rename)(README_HIDDEN, README_SRC)
+  })
+  await Promise.all([nukeP, restoreP])
+})()
--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,27 +1,27 @@
 'use strict'
 
+/**
+ * Transforms the AsciiDoc README (README.adoc) in the working directory into
+ * Markdown format (README.md) and hides the AsciiDoc README (.README.adoc).
+ */
+
 const fs = require('fs')
+const { promisify } = require('util')
+
+const README_SRC = 'README.adoc'
+const README_HIDDEN = '.' + README_SRC
+const README_DEST = 'README.md'
 
 function writeMarkdown (asciidoc) {
   const markdown = asciidoc
     .replace(/^=+(?= \w)/gm, (m) => '#'.repeat(m.length))
     .replace(new RegExp('(https?:[^\\[]+)\\[(|.*?[^\\\\])\\]', 'g'), '[$2]($1)')
-  fs.writeFile('README.md', markdown, (writeErr) => {
-    if (writeErr) throw writeErr
-  })
+  return promisify(fs.writeFile)(README_DEST, markdown)
 }
 
-// Transform README.adoc into README.md and hide README.adoc
-fs.readFile('README.adoc', 'utf8', (readErr, asciidoc) => {
-  if (readErr) {
-    fs.readFile('.README.adoc', 'utf8', (retryReadErr, retryAsciidoc) => {
-      if (retryReadErr) throw retryReadErr
-      writeMarkdown(retryAsciidoc)
-    })
-  } else {
-    fs.rename('README.adoc', '.README.adoc', (renameErr) => {
-      if (renameErr) throw renameErr
-    })
-    writeMarkdown(asciidoc)
-  }
-})
+;(async () => {
+  const readmeSrc = await promisify(fs.exists)(README_SRC).then((exists) => exists ? README_SRC : README_HIDDEN)
+  const writeP = promisify(fs.readFile)(readmeSrc, 'utf8').then((asciidoc) => writeMarkdown(asciidoc))
+  const renameP = readmeSrc === README_SRC ? promisify(fs.rename)(README_SRC, README_HIDDEN) : Promise.resolve()
+  await Promise.all([writeP, renameP])
+})()
