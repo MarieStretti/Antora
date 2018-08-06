f55007c93cf0d9306ee2bc6abf9ab8fad2ccf01b
Dan Allen <dan@opendevise.com>
2018-08-05 21:10:15 -0600

    upgrade dependencies
    - upgrade got from 8.3.2 to 9.0.0
    - upgrade fs-extra from 6.0.1 to 7.0.0
    - upgrade commander from 2.16.0 to 2.17.0
    - trim extra trailing newline from commander's help information
    - update nodegit from 0.22.1 to 0.22.2
    - upgrade convict from 4.3.1 to 4.3.2
    - upgrade eslint from 5.1.0 to 5.3.0

--- packages/cli/lib/commander/condense-help.js
+++ packages/cli/lib/commander/condense-help.js
@@ -9,11 +9,12 @@ const indentation = '  '
 Command.prototype.helpInformation = function () {
   return helpInformation
     .call(this)
+    .trimRight()
     .split(/^/m)
     .reduce((accum, line) => {
       if (line === '\n') {
-        const lastLine = accum[accum.length - 1]
-        if (lastLine === undefined || !(lastLine === '\n' || lastLine.endsWith(':\n'))) accum.push(line)
+        const prevLine = accum[accum.length - 1]
+        if (prevLine === undefined || !(prevLine === '\n' || prevLine.endsWith(':\n'))) accum.push(line)
       } else if (line.startsWith(indentation + 'Usage: ') && this.parent) {
         let ancestor = this
         let commandCtx = []
@@ -25,4 +26,5 @@ Command.prototype.helpInformation = function () {
       return accum
     }, [])
     .join('')
+    .concat('\n')
 }
