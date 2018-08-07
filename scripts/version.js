dd21481a4843e582ce1d0d84d816e45430f0b774
Dan Allen <dan@opendevise.com>
2018-08-07 04:05:04 -0600

    add script to substitute Unreleased section in CHANGELOG with version/date when releasing [skip ci]

---
+++ scripts/version.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const fs = require('fs')
+const git = require('nodegit')
+const path = require('path')
+const { promisify } = require('util')
+const { version } = require('../lerna.json')
+
+const PROJECT_ROOT_DIR = path.join(__dirname, '..')
+const CHANGELOG_FILE = path.join(PROJECT_ROOT_DIR, 'CHANGELOG.adoc')
+
+;(async () => {
+  const now = new Date()
+  const currentDate = new Date(now - (now.getTimezoneOffset() * 60000)).toISOString().split('T')[0]
+  await promisify(fs.readFile)(CHANGELOG_FILE, 'utf8').then((changelog) =>
+    promisify(fs.writeFile)(CHANGELOG_FILE, changelog.replace(/^== Unreleased$/m, `== ${version} (${currentDate})`))
+  )
+  await git.Repository.open(PROJECT_ROOT_DIR).then((repo) =>
+    repo.refreshIndex().then((index) =>
+      index.addByPath('CHANGELOG.adoc')
+        .then(() => index.write()
+        .then(() => index.writeTree())
+      )
+   ))
+})()
