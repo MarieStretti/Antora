2b5ea65944a83fbeeb86a8a8edc5e403fcf0dfbf
Dan Allen <dan@opendevise.com>
2018-09-16 17:13:30 -0600

    upgrade commander.js
    - upgrade commander to 2.18.0
    - update help grooming logic
    - convert help text for --help and --version to a sentence (capitalize and add full stop)

--- packages/cli/lib/commander/condense-help.js
+++ packages/cli/lib/commander/condense-help.js
@@ -3,28 +3,31 @@
 const { Command } = require('commander')
 
 const helpInformation = Command.prototype.helpInformation
-const indentation = '  '
 
 // TODO include common options when outputting help for a (sub)command
 Command.prototype.helpInformation = function () {
   return helpInformation
     .call(this)
-    .trimRight()
     .split(/^/m)
     .reduce((accum, line) => {
       if (line === '\n') {
         const prevLine = accum[accum.length - 1]
         if (prevLine === undefined || !(prevLine === '\n' || prevLine.endsWith(':\n'))) accum.push(line)
-      } else if (line.startsWith(indentation + 'Usage: ') && this.parent) {
-        let ancestor = this
-        let commandCtx = []
-        while ((ancestor = ancestor.parent)) commandCtx.unshift(ancestor.name())
-        accum.push('Usage: ' + commandCtx.join(' ') + line.substr(8))
+      } else if (line.startsWith('Usage: ')) {
+        if (this.parent) {
+          let ancestor = this
+          let commandCtx = []
+          while ((ancestor = ancestor.parent)) commandCtx.unshift(ancestor.name())
+          accum.push('Usage: ' + commandCtx.join(' ') + line.substr(6))
+        } else {
+          accum.push(line)
+        }
+      } else if (line.startsWith('  -v,') || line.startsWith('  -h,')) {
+        accum.push(line.trimRight().replace('  output', '  Output') + '.\n')
       } else {
-        accum.push(line.substr(indentation.length))
+        accum.push(line)
       }
       return accum
     }, [])
     .join('')
-    .concat('\n')
 }
