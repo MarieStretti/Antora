0c81e48418fc5bb0b4baaf12e8358b58a2bac55d
Dan Allen <dan@opendevise.com>
2018-12-30 03:02:42 -0700

    don't allow default task to mask delegate task

--- gulpfile.js/lib/export-tasks.js
+++ gulpfile.js/lib/export-tasks.js
@@ -1,4 +1,8 @@
 'use strict'
 
-module.exports = (...tasks) =>
-  tasks.reduce((acc, task) => (acc[task.displayName || task.name] = task) && acc, { default: tasks.shift() })
+module.exports = (...tasks) => {
+  if (!tasks.length) return {}
+  return tasks.reduce((acc, task) => (acc[task.displayName || task.name] = task) && acc, {
+    default: Object.assign(tasks[0].bind(null), { description: `=> ${tasks[0].displayName}`, displayName: 'default' }),
+  })
+}
