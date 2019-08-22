63c544776fffc72b9b799dfbb083d8202ecc530c
Dan Allen <dan@opendevise.com>
2019-08-22 17:12:55 -0600

    add convert prefix to convert function name; consolidate condition

--- packages/asciidoc-loader/lib/converter/html5.js
+++ packages/asciidoc-loader/lib/converter/html5.js
@@ -15,31 +15,29 @@ const Html5Converter = (() => {
     Opal.send(this, Opal.find_super_dispatcher(this, 'initialize', initialize), [backend, opts])
     this[$pageRefCallback] = callbacks.onPageRef
   })
-  Opal.defn(scope, '$inline_anchor', function inlineAnchor (node) {
+  Opal.defn(scope, '$inline_anchor', function convertInlineAnchor (node) {
     if (node.getType() === 'xref') {
-      if (node.getAttribute('path')) {
-        const callback = this[$pageRefCallback]
-        if (callback) {
-          const attrs = node.getAttributes()
-          if (attrs.fragment === Opal.nil) delete attrs.fragment
-          const { content, target } = callback(attrs.refid, node.getText())
-          let options
-          // NOTE if resolved target starts with #, it's an unresolved xref
-          if (target.charAt() === '#') {
-            options = Opal.hash2(['type', 'target'], { type: 'link', target })
-          } else {
-            attrs.role = attrs.role ? 'page ' + attrs.role : 'page'
-            options = Opal.hash2(['type', 'target', 'attrs'], {
-              type: 'link',
-              target,
-              attributes: Opal.hash2(Object.keys(attrs), attrs),
-            })
-          }
-          node = Opal.module(null, 'Asciidoctor').Inline.$new(node.getParent(), 'anchor', content, options)
+      let callback
+      if (node.getAttribute('path') && (callback = this[$pageRefCallback])) {
+        const attrs = node.getAttributes()
+        if (attrs.fragment === Opal.nil) delete attrs.fragment
+        const { content, target } = callback(attrs.refid, node.getText())
+        let options
+        // NOTE if resolved target starts with #, it's an unresolved xref
+        if (target.charAt() === '#') {
+          options = Opal.hash2(['type', 'target'], { type: 'link', target })
+        } else {
+          attrs.role = attrs.role ? 'page ' + attrs.role : 'page'
+          options = Opal.hash2(['type', 'target', 'attrs'], {
+            type: 'link',
+            target,
+            attributes: Opal.hash2(Object.keys(attrs), attrs),
+          })
         }
+        node = Opal.module(null, 'Asciidoctor').Inline.$new(node.getParent(), 'anchor', content, options)
       }
     }
-    return Opal.send(this, Opal.find_super_dispatcher(this, 'inline_anchor', inlineAnchor), [node])
+    return Opal.send(this, Opal.find_super_dispatcher(this, 'inline_anchor', convertInlineAnchor), [node])
   })
   return scope
 })()
